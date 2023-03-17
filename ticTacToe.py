1594ab156078b350c20c141ffe759ca7c288804d
yshashanky <812shanky@gmail.com>
2023-03-17 14:34:44 +0530

    tictactoe update

--- ticTacToe.py
+++ ticTacToe.py
@@ -8,21 +8,27 @@ clock = pygame.time.Clock()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 pygame.display.set_caption("Welcome to Tic-Tac-Toe")
 
-def drawCircle():
-    pygame.draw.circle(screen, (0, 0, 255), (150, 150), 30, 5)
+global playerFalg
+playerFalg = False
 
-def drawCross():
+def drawCircle(w, h):
+    w = ((w // 100) * 100) + 50
+    h = ((h // 100) * 100) + 50
+    pygame.draw.circle(screen, (0, 0, 255), (w, h), 30, 5)
+
+def drawCross(w, h):
+    w = ((w // 100) * 100)
+    w1 = w + 25
+    w2 = w + 75
+    h = ((h // 100) * 100)
+    h1 = h + 25
+    h2 = h + 75
+
     pygame.draw.lines(screen, (0, 255, 0), False, 
-                      [(125, 125), (175, 175)], 5)
+                      [(w1, h1), (w2, h2)], 5)
     
     pygame.draw.lines(screen, (0, 255, 0), False, 
-                      [(175, 125), (125, 175)], 5)
-    
-def screenText(player1, player2):
-    text = str(f"Player 1: {player1} | Draw: 10 | Player 2: {player2}")
-    text = my_font.render(text, True, (200, 000, 000))
-    text_rect = text.get_rect(center=(500/2, 20))
-    screen.blit(text, text_rect)
+                      [(w1, h2), (w2, h1)], 5)
 
 def background():
     pygame.draw.lines(screen, (0, 0, 0), True, 
@@ -33,10 +39,35 @@ def background():
     
     pygame.draw.lines(screen, (0, 0, 0), False, 
                       [(100, 200), (400, 200), (400, 300), (100, 300)],3)
+    
+    pygame.display.flip()
+
+def scoreCard(player1, player2, draw):
+    text = str(f"Player 1: {player1} | Draw: {draw} | Player 2: {player2}")
+    text = my_font.render(text, True, (200, 000, 000))
+    text_rect = text.get_rect(center=(500/2, 20))
+    screen.blit(text, text_rect)
+
+def playerTurn(player):
+    text = str(f"It is {player} turn...")
+    text = my_font.render(text, True, (200, 000, 000))
+    text_rect = text.get_rect(center=(500/2, 80))
+    screen.blit(text, text_rect)
+
+def play(w, h):
+    global playerFalg
+    if playerFalg == False:
+        drawCircle(w, h)
+        playerFalg = True
+    else:
+        drawCross(w, h)
+        playerFalg = False
 
 def main():
     player1 = 0
     player2 = 0
+    draw = 0
+
     running = True
 
     while running:
@@ -48,13 +79,12 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 w, h = pygame.mouse.get_pos()
                 print(w, h)
+                if w in range(100, 400) and h in range(100, 400):
+                    play(w, h)
         
         background()
-        drawCross()
-        drawCircle()
-        screenText(player1, player2)
-        pygame.display.flip()
+        # playerTurn("P")
+        # scoreCard(player1, player2, draw)
         
-
 main()
 pygame.quit()
